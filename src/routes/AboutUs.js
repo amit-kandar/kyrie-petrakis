d3004bcf3354e872b36186fa30193f495eb4bf93
Amit Kandar <kandaramit2001@gmail.com>
2024-02-24 11:30:42 +0530

    improved the about page

--- src/routes/AboutUs.js
+++ src/routes/AboutUs.js
@@ -1,31 +1,70 @@
-import React from 'react'
+import React from 'react';
+import { ABOUT_BG } from '../config/constant';
 
 function AboutUs() {
     return (
-        <section className='bg-gray-100 w-full flex justify-center flex-col items-center px-10 py-8'>
-            <div className='w-fit'>
-                <div className='w-full mb-10'>
-                    <h1 className='text-2xl text-black font-semibold font-serif text-center md:text-3xl lg:text-5xl'>Our team of legal experts</h1>
+        <section className='w-full px-5 py-16 bg-gray-800'>
+            <div className='w-full flex flex-col items-center justify-center gap-10'>
+                <div className='w-full flex flex-col justify-center items-center md:items-start gap-5 lg:gap-12 md:flex-row'>
+                    <div className='w-full h-60 sm:w-[600px] sm:h-72 md:min-w-[400px] lg:min-w-[600px] lg:min-h-72 xl:min-w-[700px] xl:min-h-96'>
+                        <img src={ABOUT_BG} alt="about us" className='w-full h-full bg-cover bg-center mix-blend-lighten' />
+                    </div>
+                    <div className='w-full flex flex-col justify-center items-center md:items-start'>
+                        <div className='mb-1 text-white'>
+                            <h1 className='text-3xl font-semibold'>About Us</h1>
+                        </div>
+                        <div className='mb-3 text-gray-100 opacity-60'>
+                            <span className='text-sm'>Sample Sub-Headline</span>
+                        </div>
+                        <div className='text-center mb-8 md:text-left lg:max-w-[500px]'>
+                            <p className='text-white text-base font-light'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure aliquid, dicta officiis ex repudiandae nulla a id voluptatem! Rerum repellendus sed asperiores optio illo. Ad a ab maxime distinctio obcaecati assumenda veritatis ipsa quam doloremque, quasi praesentium facilis consectetur ratione earum laboriosam asperiores nihil eum mollitia? Amet, aliquam obcaecati. Repellat?
+                            </p>
+                        </div>
+                        <div className='w-full flex justify-center md:justify-start'>
+                            <button className='px-8 py-3 text-white border border-gray-200 rounded-md cursor-pointer duration-200 hover:bg-gray-500 hover:scale-110'>Learn More</button>
+                        </div>
+                    </div>
                 </div>
-                <div className='mt-10 flex flex-col justify-between items-center gap-5 md:flex-row md:items-center mb-10'>
-                    <div className='bg-gray-500 flex-1 px-5 py-3 rounded-xl max-w-96 min-h-36'>
-                        <h2 className='text-xl text-white '>Experienced Attorneys</h2>
-                        <p className='text-base text-gray-300'>
-                            Meet our team of knowledgeable attorneys with
-                            expertise in diverse legal areas.
+                <div className='w-full flex justify-center items-center gap-5 flex-wrap'>
+                    <div className='w-full mb-10 flex justify-center items-center md:justify-center text-white'>
+                        <h1 className='text-2xl font-semibold md:text-3xl lg:text-4xl'>How we Work?</h1>
+                    </div>
+                    <div className='w-full h-40 flex-1 min-w-[240px] max-w-96 p-5 rounded-md bg-slate-700'>
+                        <div className='mb-3'>
+                            <h4 className='text-xl text-white'>1. Legal Consultation</h4>
+                        </div>
+                        <p className='text-gray-400 text-sm'>
+                            Connect with experienced
+                            lawyers for expert advice
+                            and guidance regarding
+                            your legal needs.
+
                         </p>
                     </div>
-                    <div className='bg-gray-500 flex-1 px-5 py-3 rounded-xl max-w-96 min-h-36'>
-                        <h2 className='text-xl text-white '>Specialized Proficiency</h2>
-                        <p className='text-base text-gray-300'>
-                            Get to know our legal experts who specialize in
-                            areas such as corporate law, immigration, and
-                            intellectual property.
+                    <div className='w-full h-40 flex-1 min-w-[240px] max-w-96 p-5 rounded-md bg-slate-700'>
+                        <div className='mb-3'>
+                            <h4 className='text-xl text-white'>2. Document Preparation</h4>
+                        </div>
+                        <p className='text-gray-400 text-sm'>
+                            Efficiently draft and
+                            customize legal
+                            documents with the
+                            assistance of our
+                            knowledgeable team.
                         </p>
                     </div>
-                </div>
-                <div className='w-full'>
-                    <button className='px-8 py-3 text-black text-lg bg-transparent border-2 border-black rounded-full'>Book Now</button>
+                    <div className='w-full h-40 flex-1 min-w-[240px] max-w-96 p-5 rounded-md bg-slate-700'>
+                        <div className='mb-3'>
+                            <h4 className='text-xl text-white'>3. Case Management</h4>
+                        </div>
+                        <p className='text-gray-400 text-sm'>
+                            Keep track of your cases,
+                            appointments, and
+                            important documents
+                            within our secure platform.
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
