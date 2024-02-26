8a9b35f33dc1ac64234edd5f98a690c709332c52
Amit Kandar <kandaramit2001@gmail.com>
2024-02-26 17:00:20 +0530

    create services component

---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Card({ image_src, title, services, link_to }) {
+    return (
+        <div className='relative group overflow-hidden w-72 h-80 flex flex-col gap-3 justify-center items-center rounded-md bg-gray-900'>
+            <img src={image_src} alt="income tax" className='w-20 h-20 shadow-xl p-3 rounded-md bg-gray-200' />
+            <div className='absolute inset-0 translate-y-[100%] group-hover:translate-y-0 bg-inherit duration-700 py-2'>
+                <div className='w-full my-8 text-center'>
+                    <h3 className='text-3xl text-white font-medium'>{title}</h3>
+                </div>
+                <div className='w-full px-8'>
+                    <ul className='list-disc pl-5 text-white'>
+                        {services.map((service, index) => (
+                            <li key={index}>{service}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className='w-full flex justify-center mt-8'>
+                    <Link to={`/services/${link_to}`} className='flex items-center font-medium cursor-pointer text-white hover:text-blue-500 hover:scale-110 hover:duration-200'>
+                        <span className='mb-1'>Know More</span>
+                        <i className='bx bx-right-arrow-alt text-2xl' ></i>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
---
+++ src/routes/Service.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { INCOME_TAX_ICO, GST_ICO, CRIMINAL_ICO, CIVIL_ICO, OTHHERS_ICO, GST } from '../config/constant';
+import Card from '../components/Card';
+
+function Service() {
+    return (
+        <section className='w-full py-12 px-6 bg-gray-700'>
+            <div className='container'>
+                <div className='w-full flex flex-col items-center gap-3 lg:gap-6'>
+                    <h1 className='text-3xl font-medium tracking-wider text-white md:text-4xl lg:text-6xl'>Services</h1>
+                    <p className='text-sm text-gray-300 tracking-wide opacity-50 text-center'>
+                        Efficient and reliable services tailored to your needs. Expert solutions, seamless communication, and superior results. Choose excellence for success.
+                    </p>
+                </div>
+                <div className='w-full grid grid-cols-1 place-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 my-8'>
+                    <Card
+                        title="Income Tax"
+                        image_src={INCOME_TAX_ICO}
+                        services={[
+                            "E-Filing",
+                            "Cases",
+                            "Accounting",
+                            "Financial Statements"
+                        ]}
+                        link_to="/income-tax"
+                    />
+                    <Card
+                        title="GST"
+                        image_src={GST_ICO}
+                        services={[
+                            "Monthly or Quarterly Return",
+                            "Reconciliation",
+                            "Accounting",
+                            "Cases"
+                        ]}
+                        link_to="/gst"
+                    />
+                    <Card
+                        title="Criminal"
+                        image_src={CRIMINAL_ICO}
+                        services={[
+                            "Bail Petition",
+                            "NI Act Cases",
+                            "Others",
+                        ]}
+                        link_to="/criminal"
+                    />
+                    <Card
+                        title="Civil"
+                        image_src={CIVIL_ICO}
+                        services={[
+                            "Property Registration",
+                            "Property Related Cases",
+                            "Accounting",
+                            "Financial Statements"
+                        ]}
+                        link_to="/civil"
+                    />
+                    <Card
+                        title="Others"
+                        image_src={OTHHERS_ICO}
+                        services={[
+                            "Patent and Trade Mark",
+                            "Incorporation of Private Limited",
+                            "Deeds and Agreements",
+                            "Power of Attorney",
+                            "etc"
+                        ]}
+                        link_to="/others"
+                    />
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Service
