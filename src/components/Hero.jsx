6dbcd77cc3c8b5db1f5d705131f08cd979abb93c
Amit Kandar <kandaramit2001@gmail.com>
2024-02-26 17:17:53 +0530

    Change Name on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HERO_BG_IMG } from '../config/constant'
+import { COMPANY_NAME, HERO_BG_IMG } from '../config/constant'
 
 function Hero() {
     return (
@@ -13,11 +13,11 @@ function Hero() {
             <div className='h-full z-10 flex flex-col justify-center items-center px-3 sm:px-5 mt-36'>
                 <div className='w-full text-white text-left' >
                     <h2 className='text-4xl font-semibold md:text-4xl lg:text-6xl'>
-                        Your LegalBuddy
+                        {COMPANY_NAME}
                     </h2>
                 </div>
                 <p className='text-left text-lg leading-7 text-zinc-300 mt-4'>
-                    Your LegalBuddy is a comprehensive legal services platform designed to make
+                    Kyrie Petrakis is a comprehensive legal services platform designed to make
                     legal assistance easily accessible. Whether you need help with contracts,
                     intellectual property, or any other legal matters, we're here to provide expert
                     guidance.
