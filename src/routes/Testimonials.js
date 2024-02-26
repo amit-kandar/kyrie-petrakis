4d0f0b16fc5de15bf31bfa415a6cb135f88af3b7
Amit Kandar <kandaramit2001@gmail.com>
2024-02-26 11:59:18 +0530

    Fix the testimonials

--- src/routes/Testimonials.js
+++ src/routes/Testimonials.js
@@ -19,13 +19,13 @@ function Testimonials() {
     }
 
     return (
-        <section className='relative w-full bg-neutral-100 h-[480px]'>
+        <section className='relative w-full bg-gray-500 min-h-[500px] lg:min-h-[600px]'>
             <div className='w-full absolute top-36'>
                 <img src={TESTIMONIALS_BG_IMG} alt="testimonials background" className='w-full' />
             </div>
             <div className='absolute top-10 w-full'>
                 <div className='text-center px-3 w-full'>
-                    <h1 className='text-center text-2xl md:text-3xl lg:text-4xl text-black font-semibold'>We serve our clients <br /> <span className='text-custom-green'>with the best of our capacity</span></h1>
+                    <h1 className='text-center text-2xl md:text-3xl lg:text-4xl text-white font-semibold'>We serve our clients <br /> <span className='text-custom-green'>with the best of our capacity</span></h1>
                 </div>
             </div>
             <div className='absolute top-44 w-full'>
