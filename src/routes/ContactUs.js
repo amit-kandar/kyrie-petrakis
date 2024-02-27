fadc84c7dfa450cea443183d82636156d864a01f
Amit Kandar <kandaramit2001@gmail.com>
2024-02-27 13:31:31 +0530

    Improved the contact us page design

--- src/routes/ContactUs.js
+++ src/routes/ContactUs.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
+import LOCATION from '../assets/marker.svg';
+import USER from '../assets/user.svg';
+import { TESTIMONIALS_BG_IMG } from '../config/constant'
 
 function ContactUs() {
+    // const [selectedService, setSelectedService] = useState('');
+    // const [isOpen, setIsOpen] = useState(false)
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -19,30 +24,41 @@ function ContactUs() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Add your form submission logic here
         console.log('Form submitted:', formData);
-        // You may want to send the data to a server or perform other actions
     };
+
+    // const services = [
+    //     'Income Tax',
+    //     'GST',
+    //     'Civil Law',
+    //     'Criminal Law',
+    //     'Other Legal Services'
+    // ];
+
+    // const handleServiceChange = (event) => {
+    //     setSelectedService(event.target.value);
+    // };
+
+    // const handleDropdown = () => {
+    //     setIsOpen(prev => !prev);
+    // }
+
+
+
     return (
-        <section className="w-full py-10 px-5 lg:px-20 bg-gray-700">
-            <div className='flex flex-col justify-around gap-5 md:gap-x-16 md:flex-row'>
+        <section className="w-full py-10 px-5 lg:px-12 xl:px-16 bg-gray-700">
+            <div className='flex flex-col justify-around gap-5 md:gap-x-8 md:flex-row'>
                 <div className="w-full max-w-[600px] flex-1 mx-auto mt-8 p-6 bg-gray-800 rounded-md shadow-lg">
                     <div className='w-full flex justify-center md:justify-start'>
                         <h2 className="text-3xl md:text-4xl font-semibold mb-4 text-white">Send us a message</h2>
                     </div>
                     <form onSubmit={handleSubmit}>
-                        <div className="max-w-3xl mx-auto flex flex-wrap items-start gap-4 my-8">
+                        <div className="max-w-3xl mx-auto flex flex-wrap items-start gap-4 my-2">
                             <div className="grid gap-1.5 w-full">
-                                <label className="block text-sm font-medium text-gray-100" htmlFor="first-name">
-                                    First name
+                                <label className="block text-sm font-medium text-gray-100" htmlFor="name">
+                                    Name
                                 </label>
-                                <input id="first-name" placeholder="Enter your first name" className="bg-transparent border-2 border-zinc-500 rounded-md py-3 pl-5 outline-none text-gray-100 focus:border-zinc-200 duration-200" onCanPlay={handleChange} />
-                            </div>
-                            <div className="grid gap-1.5 w-full">
-                                <label className="block text-sm font-medium text-gray-100" htmlFor="last-name">
-                                    Last name
-                                </label>
-                                <input id="last-name" placeholder="Enter your last name" className="bg-transparent border-2 border-zinc-500 rounded-md py-3 pl-5 outline-none text-gray-100 focus:border-zinc-200 duration-200" onCanPlay={handleChange} />
+                                <input id="name" placeholder="Enter your name" className="bg-transparent border-2 border-zinc-500 rounded-md py-3 pl-5 outline-none text-gray-100 focus:border-zinc-200 duration-200" onCanPlay={handleChange} />
                             </div>
                             <div className="grid gap-1.5 w-full">
                                 <label className="block text-sm font-medium text-gray-100" htmlFor="email">
@@ -50,12 +66,20 @@ function ContactUs() {
                                 </label>
                                 <input id="email" placeholder="Enter your email" type="email" className="bg-transparent border-2 border-zinc-500 rounded-md py-3 pl-5 outline-none text-gray-100 focus:border-zinc-200 duration-200" onCanPlay={handleChange} />
                             </div>
-                            <div className="grid gap-1.5 w-full">
-                                <label className="block text-sm font-medium text-gray-100" htmlFor="email">
-                                    Select Service
-                                </label>
-                                <input id="service" placeholder="Enter service name" type="text" className="bg-transparent border-2 border-zinc-500 rounded-md py-3 pl-5 outline-none text-gray-100 focus:border-zinc-200 duration-200" onCanPlay={handleChange} />
-                            </div>
+                            {/* <div className="relative grid gap-1.5 w-full" onClick={handleDropdown}>
+                                <label className="block text-sm font-medium text-gray-100" htmlFor="service">Service:</label>
+                                <div className="bg-transparent border-2 border-zinc-500 rounded-md py-2 px-5 outline-none text-gray-300 focus:border-zinc-200 duration-200">
+                                    <div className='flex justify-between items-center'>
+                                        <span>Select Service</span>
+                                        {
+                                            isOpen ? <i className='bx bx-chevron-up text-3xl'></i> : <i className='bx bx-chevron-down text-3xl'></i>
+                                        }
+                                    </div>
+                                    <div className={` ${isOpen ? "block" : "hidden"} absolute w-full h-20 bg-current inset-0 translate-y-[84px] rounded-md`}>
+
+                                    </div>
+                                </div>
+                            </div> */}
                             <div className="grid gap-1.5 w-full">
                                 <label className="block text-sm font-medium text-gray-100" htmlFor="subject">
                                     Subject
@@ -76,20 +100,28 @@ function ContactUs() {
                 </div>
                 <div className='flex flex-col flex-1 gap-6'>
                     <div className='w-full mt-10'>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5209.974580116529!2d88.40267528884736!3d22.578790820802357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a027677c1c8a03b%3A0xe1bc02832a8f64a4!2sMani%20Square!5e0!3m2!1sen!2sin!4v1708165745892!5m2!1sen!2sin" className='w-full h-[200px] sm:h-80 md:h-96 rounded-md' allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title='map' color='dark' ></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5209.974580116529!2d88.40267528884736!3d22.578790820802357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a027677c1c8a03b%3A0xe1bc02832a8f64a4!2sMani%20Square!5e0!3m2!1sen!2sin!4v1708165745892!5m2!1sen!2sin" className='w-full h-[300px] sm:h-90 md:h-96 rounded-md' allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title='map' color='dark' ></iframe>
                     </div>
-                    <div className='w-full flex flex-col justify-between gap-2 sm:flex-row'>
-                        <div className='p-3'>
-                            <p className='text-sm font-medium text-white opacity-70'>Main Office<br />123 Anywhere St.<br />Any City ST 12345</p>
-                        </div>
-                        <div className='p-3'>
-                            <div className='flex flex-row items-center gap-3 text-white opacity-70'>
-                                <i className='bx bx-phone-call'></i>
-                                <span className='text-sm'>(123) 456-7890</span>
+                    <div className='w-full flex flex-wrap justify-around gap-2'>
+                        <div className='p-3 flex items-center flex-col gap-4'>
+                            <div className='w-16 h-16 flex justify-center items-center rounded-full border-2 border-gray-100'>
+                                <img src={LOCATION} alt="location" className='w-6 h-6' />
                             </div>
-                            <div className='flex flex-row items-center gap-3 text-white opacity-70'>
-                                <i className='bx bx-envelope text-lg'></i>
-                                <span className='text-sm'>info@example.com</span>
+                            <p className='text-sm text-center font-medium text-white opacity-70'>Main Office<br />123 Anywhere St.<br />Any City ST 12345</p>
+                        </div>
+                        <div className='p-3 flex items-center flex-col gap-4'>
+                            <div className='w-16 h-16 flex justify-center items-center rounded-full border-2 border-gray-100'>
+                                <img src={USER} alt="user" className='w-6 h-6' />
+                            </div>
+                            <div>
+                                <div className='flex flex-row items-center gap-3 text-white opacity-70'>
+                                    <i className='bx bx-phone-call'></i>
+                                    <span className='text-sm'>(123) 456-7890</span>
+                                </div>
+                                <div className='flex flex-row items-center gap-3 text-white opacity-70'>
+                                    <i className='bx bx-envelope text-lg'></i>
+                                    <span className='text-sm'>info@example.com</span>
+                                </div>
                             </div>
                         </div>
                     </div>
