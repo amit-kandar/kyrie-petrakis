5dbeb8c97b047943733152cdffc074171f361735
Amit Kandar <kandaramit2001@gmail.com>
2024-02-28 16:46:09 +0530

    Added sliding background image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import { COMPANY_NAME, HERO_BG_IMG } from '../config/constant'
+import { COMPANY_NAME, HERO_BG_IMG, HERO_BG_IMG1, HERO_BG_IMG2, HERO_BG_IMG3, HERO_BG_IMG4 } from '../config/constant'
 import { Link } from 'react-router-dom';
+import Carousel from 'nuka-carousel';
 
 function Hero() {
+    const bg_img = [
+        HERO_BG_IMG,
+        HERO_BG_IMG1,
+        HERO_BG_IMG2,
+        HERO_BG_IMG3,
+        HERO_BG_IMG4
+    ]
     return (
         <section className='relative min-h-screen flex p-5 md:p-10'>
-            <div
-                className="absolute inset-0 w-full h-full bg-cover bg-center"
-                style={{ backgroundImage: `url(${HERO_BG_IMG})` }}
-            >
+            <div className="absolute inset-0 w-full h-full">
+                <Carousel autoplay autoplayInterval={3000} slidesToScroll={1} wrapAround={true} withoutControls>
+                    {
+                        bg_img.map((item, index) => {
+                            return (
+                                <img src={item} key={index} alt="hero background" className='w-full min-h-screen bg-cover bg-center object-cover bg-no-repeat' />
+                            )
+                        })
+                    }
+                </Carousel>
                 <div className="absolute inset-0 bg-black opacity-50"></div>
             </div>
-            <div className='max-w-7xl h-full z-10 flex flex-col justify-center items-center px-3 sm:px-5 mt-36'>
+            <div className='max-w-7xl h-full z-10 flex flex-col justify-center items-center px-3 sm:px-5 mt-16 md:mt-36'>
                 <div className='w-full text-white text-left' >
                     <h2 className='text-4xl font-semibold md:text-4xl lg:text-6xl'>
                         {COMPANY_NAME}
