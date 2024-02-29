4e6b3542d7d70f3d9842d35d39836b482fa5f0f5
Amit Kandar <kandaramit2001@gmail.com>
2024-02-29 21:15:42 +0530

    Fixed the service schema

--- src/schema/serviceSchema.js
+++ src/schema/serviceSchema.js
@@ -4,7 +4,7 @@ const serviceSchema = yup.object().shape({
     first_name: yup.string().required('First name is required'),
     last_name: yup.string().required('Last name is required'),
     email: yup.string().email('Invalid email'),
-    phone: yup.string().matches(/^\d{10}$/, 'Invalid phone number').required('Phone is required'),
+    phone_number: yup.string().matches(/^\d{10}$/, 'Invalid phone number').required('Phone is required'),
     service: yup.string().required('Service is required'),
     subject: yup.string().required('Subject is required'),
     message: yup.string().required('Message is required'),
